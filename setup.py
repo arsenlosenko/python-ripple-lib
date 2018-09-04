5e650f02767e25277c8ce70c699618640bc346ca
Arsen Losenko <arsenlosenko@gmail.com>
2018-09-04 17:30:02 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='python-ripple-rpc',
+    version='1.0',
+    packages=['api', 'test'],
+    url='https://github.com/arsenlosenko/python-ripple-rpc',
+    license='MIT',
+    author='Arsen Losenko',
+    author_email='arsenlosenko@gmail.com',
+    description='Ripple JSON-RPC API wrapper for python',
+    long_description=open('readme.md').read(),
+    keywords='ripple rippled json-rpc',
+    classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Utilities',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.5'
+
+    ]
+)
